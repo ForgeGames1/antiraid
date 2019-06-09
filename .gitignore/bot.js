44e2bc0a64943299a826352d268f6be999ecb19f
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2019-06-09 02:24:41 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-  message.channel.sendMessage("test");
+  if(message.content === "test123"){
+    message.delete(1);
+    message.channel.sendMessage("message suprimé");
+  }
 });
 
 client.login('NTg3MDcwOTI2MjQ0NDEzNDQw.XPxOaw.ssAjKKa5-cNZyoTA63FS3rPSbMc');
