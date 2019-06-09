1c714eabe3b2e8c4ed86a0a49efceaec4df64557
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2019-06-09 02:48:28 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-  if(message.content.startsWith("test"){
      message.channel.sendMessage("t");
-     }
 });
 
 
