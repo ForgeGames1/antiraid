395f86a127f73983ff5017c4d1a677236da33343
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2019-06-09 02:34:20 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,13 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const AntiRaid = "false";
 
 client.on('message', message => {
   message.channel.sendMessage("a");
   if(message.content.startWith("..antiraid")){
-      message.channel.sendMessage("AntiRaid " + AntiRaid);
       message.channel.sendMessage("t");
-      AntiRaid = AntiRaid : "false" ? "true";
   }
 });
 
