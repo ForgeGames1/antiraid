646825e0aabdac50b2cb2c59283fe1cb29272586
ForgeGames1 <34386594+ForgeGames1@users.noreply.github.com>
2019-06-11 21:33:01 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const clientFunRadio = new Discord.Client();
 const clientNRJ = new Discord.Client();
-const connection = null;
-const connection2 = null;
 
 clientFunRadio.login('NTg4MDYwMTAzODUzMjExNjU5.XP_nsw.XWJUmX7sp4IWGQ_muRv6SR_xtoY');
 clientNRJ.login('NTg4MDYwNTU4MTIxNTAwNjc1.XP_oHA.vy02nb4NZHmVzW8dYUUb1-Enhlk');
@@ -15,24 +13,6 @@ clientNRJ.on("ready", () => {
     clientNRJ.user.setActivity("Manage by ForgeGames", { type: "STREAMING", url: "https://www.twitch.tv/NRJ" })
 })
 
-clientFunRadio.on('message', async message =>{
-  if (!message.guild) return;
-
-  if (message.content === '/leave FunRadio') {
-    if(message.author.isOwner  || message.author.id === '586831689033187355'){
-      if(clientFunRadio.member.voiceChannel){
-        connection.destroy();
-      }
-    }
-  }else if (message.content === '/leave NRJ') {
-    if(message.author.isOwner  || message.author.id === '586831689033187355'){
-      if(clientNRJ.member.voiceChannel){
-        connection1.destroy();
-      }
-    }
-  }
-})
-
 
 clientFunRadio.on('message', async message => {
 
@@ -41,7 +21,7 @@ clientFunRadio.on('message', async message => {
   if (message.content === '/join FunRadio') {
     if(message.author.isOwner  || message.author.id === '586831689033187355'){
     if (message.member.voiceChannel) {
-      connection = await message.member.voiceChannel.join();
+      const connection = await message.member.voiceChannel.join();
 
       connection.playStream('http://streaming.radio.funradio.fr/fun-1-48-192', {
   volume: 0.1,
@@ -63,7 +43,7 @@ clientNRJ.on('message', async message => {
   if (message.content === '/join NRJ') {
     if(message.author.isOwner || message.author.id === '586831689033187355'){
       if (message.member.voiceChannel) {
-        connection1 = await message.member.voiceChannel.join();
+        const connection1 = await message.member.voiceChannel.join();
 
         connection1.playStream('http://cdn.nrjaudio.fm/audio1/fr/30001/mp3_128.mp3?origine=fluxradios', {
             volume: 0.1,
